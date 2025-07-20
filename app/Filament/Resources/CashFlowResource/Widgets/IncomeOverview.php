71ab733ddd5f6b03f119a94560dee79ba8275ecd
Alishlah <alishlah56@gmail.com>
2025-07-20 21:15:41 +0700

    ganti

--- app/Filament/Resources/CashFlowResource/Widgets/IncomeOverview.php
+++ app/Filament/Resources/CashFlowResource/Widgets/IncomeOverview.php
@@ -16,22 +16,22 @@ class IncomeOverview extends BaseWidget
     {
         return ListCashFlows::class;
     }
-    
+
     protected function getStats(): array
     {
         return [
-            Stat::make('Total Modal', 'Rp ' . number_format($this->getPageTableQuery()->where('type','income')->where('source','like','%capital%')->sum('amount') ?? 0 ,0,",","."))
-            ->description('Total Capital'),
-            Stat::make('Total Uang Masuk', 'Rp ' . number_format($this->getPageTableQuery()->where('type','income')->sum('amount') ?? 0 ,0,",","."))
-            ->description('Cash Inflow')
-            ->descriptionIcon('heroicon-m-arrow-trending-up',IconPosition::Before)
-            ->color('success'),
-            Stat::make('Total Uang Keluar', 'Rp ' . number_format($this->getPageTableQuery()->where('type','expense')->sum('amount') ?? 0 ,0,",","."))
-            ->description('Cash Outflow')
-            ->descriptionIcon('heroicon-m-arrow-trending-down',IconPosition::Before)
-            ->color('danger'),
-            Stat::make('Total Uang Toko', 'Rp ' . number_format($this->getPageTableQuery()->where('type','income')->sum('amount') - $this->getPageTableQuery()->where('type','expense')->sum('amount') ?? 0 ,0,",","."))
-            ->description('masuk : Rp ' . number_format($this->getPageTableQuery()->where('type','income')->sum('amount') ?? 0 ,0,",",".") . ' - Keluar : Rp ' . number_format($this->getPageTableQuery()->where('type','expense')->sum('amount') ?? 0 ,0,",",".")),
+            Stat::make('Total Modal', 'Rp ' . number_format($this->getPageTableQuery()->where('type', 'income')->where('source', 'like', '%capital%')->sum('amount') ?? 0, 0, ",", "."))
+                ->description('Total Capital'),
+            Stat::make('Total Uang Masuk', 'Rp ' . number_format($this->getPageTableQuery()->where('type', 'income')->sum('amount') ?? 0, 0, ",", "."))
+                ->description('Cash Inflow')
+                ->descriptionIcon('heroicon-m-arrow-trending-up', IconPosition::Before)
+                ->color('success'),
+            Stat::make('Total Uang Keluar', 'Rp ' . number_format($this->getPageTableQuery()->where('type', 'expense')->sum('amount') ?? 0, 0, ",", "."))
+                ->description('Cash Outflow')
+                ->descriptionIcon('heroicon-m-arrow-trending-down', IconPosition::Before)
+                ->color('danger'),
+            Stat::make('Total Uang Toko', 'Rp ' . number_format($this->getPageTableQuery()->where('type', 'income')->sum('amount') - $this->getPageTableQuery()->where('type', 'expense')->sum('amount') ?? 0, 0, ",", "."))
+                ->description('masuk : Rp ' . number_format($this->getPageTableQuery()->where('type', 'income')->sum('amount') ?? 0, 0, ",", ".") . ' - Keluar : Rp ' . number_format($this->getPageTableQuery()->where('type', 'expense')->sum('amount') ?? 0, 0, ",", ".")),
         ];
     }
 }
